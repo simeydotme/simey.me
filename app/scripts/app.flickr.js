da6ccd7c73df2fe9c4c6e0c8556e9f7328be936e
simeydotme <simey.me@gmail.com>
2014-04-21 23:16:19 +0100

    init flickr stuff

---
+++ app/scripts/app.flickr.js
@@ -0,0 +1,107 @@
+"use strict";
+
+var app = app || {};
+
+
+
+app.flickr = {
+
+    init: function() {
+
+        this.imageCount = 8;
+        this.cycleSpeed = 5000;
+
+        this.template = $("#flickrTemplate").text();
+        this.$photobox = $(".photo-box");
+
+        this.imageCollection = [];
+
+        // please don't use my API key in your projects, it'll slow down
+        // your website as well as mine. Please get your own from 
+        // https://www.flickr.com/services/apps/create/
+        // it's totally free :)
+
+        this.apiKey = "dd8e97ef9c1283583dcaf92b9ba80170";
+        this.photoset = "72157634483069405";
+        this.collectionURL =
+            "http://api.flickr.com/services/rest/"+
+                "?format=json&jsoncallback="+
+                    "?&method=flickr.photosets.getPhotos&api_key="+this.apiKey+"&photoset_id="+this.photoset;
+
+
+        this.getCollection( this.collectionURL );
+        
+
+    },
+
+    getCollection: function( collectionURL ) {
+
+        $.getJSON( collectionURL )
+
+            .done( function(data) {
+
+                app.flickr.imageCollection = data.photoset.photo.reverse();
+                app.flickr.initPhotos();
+
+            });
+
+
+    },
+
+    generateTemplate: function( photo ) {
+
+        var src = "http://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+"_z.jpg";
+        
+        var html =
+            this.template
+                .replace("{{title}}", photo.title._content )
+                .replace("{{alt}}", photo.title._content )
+                .replace("{{url}}", photo.urls.url[0]._content )
+                .replace("{{src}}", src );
+
+        var $html = $(html);
+
+        $html
+            .find(".photo-box__link")
+            .css("background-image", "url("+src+")" )
+            .end()
+            .find(".photo-box__photo")
+            .hide();
+
+        return $html;
+
+    },
+
+    getPhoto: function( number ) {
+
+        var photoID = app.flickr.imageCollection[number].id;
+
+        var photoURL =
+            "http://api.flickr.com/services/rest/"+
+                "?format=json&jsoncallback="+
+                    "?&method=flickr.photos.getInfo&api_key="+this.apiKey+"&photo_id="+photoID;
+
+        return $.getJSON( photoURL );
+
+    },
+
+    renderPhoto: function( photo ) {
+
+        photo.done( function( data ) {
+
+            var $html = app.flickr.generateTemplate( data.photo );
+            $html.appendTo( app.flickr.$photobox );
+
+        });
+
+    },
+
+    initPhotos: function() {
+
+        for( var i = 0; i < this.imageCount; i++ ) {
+            this.renderPhoto( this.getPhoto( i ));
+        }
+
+    }
+
+};
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,283 +1,57 @@
 "use strict";
 
-var app = {
+var app = app || {};
+
+
+$(function(){
+    app.init();
+});
+
+
+app.init = function() {
+
+    app.setup.init();
+    app.waypoints();
+
+    app.nav();
+    app.header();
+    app.github();
+
+    app.window();
+    app.background();
+
+};
+
+
+app.setup = {
 
     init: function() {
 
         app.setup.unorphanize();
         app.setup.prettify();
-        app.setup.waypoints();
-
-        app.nav();
-        app.header();
-        app.about();
-        app.github();
-
-        app.window();
-        app.background();
+        app.setup.clipboard();
 
     },
 
-    setup: {
-
-        prettify: function() {
-            window.prettyPrint();
-        },
-
-        unorphanize: function() {
-            $(".kill-orphans").unorphanize();
-        },
-
-        waypoints: function() {
-
-            $(".js-nav-waypoint").waypoint({
-
-                offset: function() {
-                    return $(".nav__list").outerHeight();
-                },
-                handler: function() {
-                    $(".nav__link").filter("[href=#" +this.id+ "]").trigger("activate");
-                }
-            });
-
-            $(".js-nav-header-waypoint").waypoint({
-
-                offset: -10,
-                handler: function() {
-                    $(".nav__link").trigger("deactivate");
-                    window.location.hash = "";
-                }
-            });
-
-        }
-
+    prettify: function() {
+        window.prettyPrint();
     },
 
-    helpers: {
-
-        getTransitionSpeed: function( $el , returnArray ) {
-
-            var speed = $el.css("transition-duration").split(","),
-                speedLength = speed.length;
-
-            for( var i = 0; i < speedLength; i++) {
-
-                var multiplier = 1000;
-
-                speed[i] =
-                    speed[i]
-                        .replace(" ","")
-                        .replace("s","");
-
-                speed[i] = parseFloat( speed[i] ) * multiplier;
-
-            }
-
-            if( returnArray ) {
-                return speed;
-            } else {
-                return Math.max.apply(Math, speed);
-            }
-
-        },
-
-        smoothScroll: function( event, hash , noHashUpdate ) {
-
-            event.preventDefault();
-
-            var to = 0,
-                $doc = $(document),
-                $window = $(window),
-                $nav = $("nav"),
-                $el = $(hash);
-
-            if( $el.exists() ) {
-
-                if ($el.offset().top > ($doc.height() - $window.height())) {
-                    to = $doc.height() - $window.height();
-                } else {
-                    to = $el.offset().top - $nav.outerHeight();
-                }
-
-                if( to <= $window.scrollTop() ) {
-                    to -= 10;
-                } else {
-                    to += 10;
-                }
-
-            }
-            
-            $("html,body")
-                .stop()
-                .animate({ scrollTop: to }, 300, function() {
-                    if(!noHashUpdate) {
-                        window.location.hash = hash;
-                    }
-                });
-
-        }
-
+    unorphanize: function() {
+        $(".kill-orphans").unorphanize();
     },
 
-    window: function() {
+    clipboard: function() {
 
-        $(window).on({
+        $(".copy-to-clipboard").on("mouseover", function(){
 
-            "resize.sections": function() {
-
-                var h = $(window).height();
-
-                $(".header").height( h );
-                $("section").css("min-height", h * 0.75 );
-                $("body").css("padding-bottom", h * 0.5 );
-
-            },
-
-            "hashchange.scrolly": function(e) {
-
-                if( new RegExp("#_github\/").test( window.location.hash ) ) {
-
-                    $(window).trigger("sublimetab", window.location.hash );
-
-                } else {
-
-                    app.helpers.smoothScroll( e , window.location.hash );
-
-                }
-
-            }
+            var range = document.createRange();
+            range.selectNodeContents($(this)[0]);
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
 
         });
-
-        $(window)
-            .trigger("resize.sections")
-            .trigger("hashchange.scrolly");
-
-        if( new RegExp("#_github\/").test( window.location.hash ) ) {
-
-            $(window).scrollTop( $("#_github").offset().top + 20 );
-
-        }
-
-    },
-
-    background: function() {
-
-        var $body = $("body"),
-            bgClasses = $body.data("classes"),
-            bgClassArray = bgClasses.split(" ");
-
-        setTimeout(function(){
-            $body.addClass( bgClassArray[ Math.floor( Math.random() * bgClassArray.length ) ] );
-        }, 2500 );
-
-    },
-
-    nav: function() {
-
-        var $items = $(".nav__item"),
-            $links = $(".nav__link"),
-            activeClass = "active";
-
-        $links.on("click", function(e) {
-
-            var $this = $(this),
-                target = $this.attr("href");
-
-            app.helpers.smoothScroll( e , target );
-
-        });
-
-        $links.on("activate", function() {
-
-            $items.removeClass( activeClass );
-            $(this).closest( $items ).addClass( activeClass );
-
-        });
-
-        $links.on("deactivate", function() {
-
-            $(this).closest( $items ).removeClass( activeClass );
-
-        });
-
-    },
-
-    header: function() {
-
-        var $headerDev = $(".header-dev"),
-            openClass = "header-dev--open",
-            slowClass = "header-dev--slow";
-
-        setTimeout(function(){
-
-            $headerDev
-                .addClass( slowClass )
-                .removeClass( openClass );
-
-            setTimeout(function(){
-
-                $headerDev.removeClass( slowClass );
-
-            }, 1500 );
-
-        }, 1000 );
-
-        $headerDev.on("click", function() {
-            $headerDev.trigger("playAnimation");
-        });
-
-        $headerDev.on("playAnimation", function() {
-
-            $headerDev.toggleClass( openClass );
-
-        });
-
-
-    },
-
-    about: function() {
-
-
-    },
-
-    github: function() {
-
-        var activeClass = "active",
-            $tabs = $(".sublime-tabs__tab"),
-            $links = $(".sublime-tabs__link"),
-            $projects = $(".sublime-project"),
-            $footer = $(".sublime__footer"),
-            $title = $(".sublime__title"),
-            titleText = $title.text();
-
-        $(window).on("sublimetab", function(e,href) {
-
-            var $link = $links.filter("[href=" +href.replace("/","\\/")+ "]"),
-                $tab = $link.closest(".sublime-tabs__tab");
-
-            $tab.trigger("activateTab",href);
-
-        });
-
-        $tabs.on("activateTab", function(e,href) {
-
-            href = href.split("/");
-
-            var $tab = $(this),
-                $project = $projects.filter("[data-project-name="+ href[href.length-1] +"]");
-
-            $tabs.removeClass( activeClass );
-            $tab.addClass( activeClass );
-            $projects.removeClass( activeClass );
-            $project.addClass( activeClass );
-
-            $title.text( $tab.children(".sublime-tabs__link").text() + " - " + titleText);
-            $footer.text( $tab.children(".sublime-tabs__link").data("type") );
-
-        });
-
-        $tabs.first().trigger("activateTab", $links.first().attr("href") );
 
     }
 
@@ -286,10 +60,112 @@ var app = {
 
 
 
+app.window = function() {
 
-$(function(){
+    $(window).on({
 
-    app.init();
+        "resize.sections": function() {
 
-});
+            var h = $(window).height();
 
+            $(".header").height( h );
+            $("section").css("min-height", h * 0.75 );
+            $("body").css("padding-bottom", h * 0.5 );
+
+        },
+
+        "hashchange": function() {
+
+            if( new RegExp("#_github\/").test( window.location.hash ) ) {
+
+                $(window).trigger("sublimetab", window.location.hash );
+
+            }
+
+        }
+
+    });
+
+    $(window)
+        .trigger("resize.sections")
+        .trigger("hashchange");
+
+    if( new RegExp("#_github\/").test( window.location.hash ) ) {
+        $(window).scrollTop( $("#_github").offset().top + 20 );
+    }
+
+};
+
+
+
+
+app.background = function() {
+
+    var $body = $("body"),
+        bgClasses = $body.data("classes"),
+        bgClassArray = bgClasses.split(" ");
+
+    $body.addClass(
+        bgClassArray[ Math.floor( Math.random() * bgClassArray.length ) ]
+    );
+
+};
+
+
+
+
+app.nav = function() {
+
+    var $items = $(".nav__item"),
+        $links = $(".nav__link"),
+        activeClass = "active";
+
+    $links.on("activate", function() {
+
+        $items.removeClass( activeClass );
+        $(this).closest( $items ).addClass( activeClass );
+
+    });
+
+    $links.on("deactivate", function() {
+
+        $(this).closest( $items ).removeClass( activeClass );
+
+    });
+
+};
+
+
+
+
+app.header = function() {
+
+    var $headerDev = $(".header-dev"),
+        openClass = "header-dev--open",
+        slowClass = "header-dev--slow";
+
+    setTimeout(function(){
+
+        $headerDev
+            .addClass( slowClass )
+            .removeClass( openClass );
+
+        setTimeout(function(){
+
+            $headerDev.removeClass( slowClass );
+
+        }, 1500 );
+
+    }, 1000 );
+
+    $headerDev.on("click", function() {
+        $headerDev.trigger("playAnimation");
+    });
+
+    $headerDev.on("playAnimation", function() {
+
+        $headerDev.toggleClass( openClass );
+
+    });
+
+};
