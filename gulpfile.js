c32486a063639909f3dacc722b085889f0cd61b3
simeydotme <simey.me@gmail.com>
2015-07-09 02:17:51 +0800

    Add teh SVGs, update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,56 @@
 
     var gulp = require("gulp"),
         sass = require("gulp-sass"),
-        clean = require("gulp-clean");
+        clean = require("gulp-clean"),
+        livereload = require("gulp-livereload"),
+        replace = require("gulp-replace"),
+        autoprefixer = require("gulp-autoprefixer");
 
 
-    gulp.task("default", ["copy:html", "clean:sass", "sass"], function() {
+    gulp.task("default", ["html", "img", "sass", "prefix"], function() {
 
         console.log("done");
 
     });
 
 
-    gulp.task("copy:html", function() {
+    gulp.task("html", function() {
+
+        var i = /<i data-icon="(.*)"><\/i>/g,
+            o = "<svg class=\"$1\"><use xlink:href=\"img/svgdefs.svg#$1\"></use></svg>";
 
         gulp
-            .src("./app/index.html")
+            .src(["./app/index.html"])
+            .pipe( replace( i, o ) )
             .pipe( gulp.dest("./dist") );
 
     });
+
+    gulp.task("img", ["clean:img"], function() {
+
+        gulp.src("./app/img/**/*")
+            .pipe( gulp.dest("./dist/img") );
+
+    });
+
+    gulp.task("sass", ["clean:sass"], function() {
+
+        return gulp
+            .src("./app/css/**/*.scss")
+            .pipe( sass().on("error", sass.logError ) )
+            .pipe( autoprefixer("last 5 versions") )
+            .pipe( gulp.dest("./dist/css") )
+            .pipe( livereload() );
+
+    });
+
+
+
+
+
+
+
+
 
     gulp.task("clean:sass", function() {
 
@@ -29,17 +62,32 @@
 
     });
 
-    gulp.task("sass", function() {
+    gulp.task("clean:img", function() {
 
-        gulp
-            .src("./app/css/**/*.scss")
-            .pipe( sass().on("error", sass.logError ))
-            .pipe( gulp.dest("./dist/css") );
+        return gulp
+            .src("./dist/img", { read: false })
+            .pipe( clean() );
 
     });
 
+
+
+
+
+
+
+
+
+
     gulp.task("watch:sass", function() {
 
-        gulp.watch("./app/css/**/*.scss", [ "clean:sass", "sass" ]);
+        livereload.listen();
+        gulp.watch("./app/css/**/*.scss", [ "sass" ]);
+
+    });
+
+    gulp.task("watch:html", function() {
+
+        gulp.watch("./app/**/*.html", [ "html" ]);
 
     });
